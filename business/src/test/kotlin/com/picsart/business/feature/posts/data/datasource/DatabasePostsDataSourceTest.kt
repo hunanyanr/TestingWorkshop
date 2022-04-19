f423574b27c16b6d5dc9071540ae7b9afa382e20
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2022-04-19 14:03:38 +0200

    fix(workshop): Fix default code
    Mostly reformat or changing return types

--- business/src/test/kotlin/com/picsart/business/feature/posts/data/datasource/DatabasePostsDataSourceTest.kt
+++ business/src/test/kotlin/com/picsart/business/feature/posts/data/datasource/DatabasePostsDataSourceTest.kt
@@ -1,8 +1,6 @@
 package com.picsart.business.feature.posts.data.datasource
 
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
 
 class DatabasePostsDataSourceTest : BehaviorSpec({
 
@@ -10,7 +8,6 @@ class DatabasePostsDataSourceTest : BehaviorSpec({
     * - it returns either Result.success or Result.failure
     * - if there is no stored data it returns a DataEmpty Failure
     * - if there is stored data then the data source returns it
-    *   - data source used a list as a storage mechanism
-    */
+    * - DataSource does not persist data over recreation */
 
 })
