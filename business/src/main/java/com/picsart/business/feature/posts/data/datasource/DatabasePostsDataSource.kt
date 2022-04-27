403cd386623c4c9ac8f164fb52d0999514af351c
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2022-04-27 13:52:50 +0200

    feature(workshop): Feature step 3 implemented

--- business/src/main/java/com/picsart/business/feature/posts/data/datasource/DatabasePostsDataSource.kt
+++ business/src/main/java/com/picsart/business/feature/posts/data/datasource/DatabasePostsDataSource.kt
@@ -1,7 +1,9 @@
 package com.picsart.business.feature.posts.data.datasource
 
 import arrow.core.Either
+import arrow.core.computations.either
 import com.picsart.business.arch.StoreDataSource
+import com.picsart.business.arch.error.Empty
 import com.picsart.business.arch.error.Failure
 import com.picsart.business.feature.posts.data.model.PostDBO
 
@@ -10,9 +12,17 @@ import com.picsart.business.feature.posts.data.model.PostDBO
  */
 internal class DatabasePostsDataSource : StoreDataSource<PostDBO> {
 
-    private val instance: MutableList<PostDBO> = mutableListOf()
+    private val posts: MutableList<PostDBO> = mutableListOf()
 
-    override suspend fun invoke(): Either<Failure,List<PostDBO>> = TODO()
+    override suspend fun invoke(): Either<Failure, List<PostDBO>> = either {
+        ensure(posts.isNotEmpty()) { Empty }
+        posts.toList()
+    }
 
-    override suspend fun invoke(put: List<PostDBO>): Either<Failure, List<PostDBO>> = TODO()
+    override suspend fun invoke(put: List<PostDBO>): Either<Failure, List<PostDBO>> = either {
+        posts.apply {
+            clear()
+            addAll(put)
+        }
+    }
 }
--- business/src/test/kotlin/com/picsart/business/feature/posts/data/datasource/DatabasePostsDataSourceTest.kt
+++ business/src/test/kotlin/com/picsart/business/feature/posts/data/datasource/DatabasePostsDataSourceTest.kt
@@ -1,13 +1,47 @@
 package com.picsart.business.feature.posts.data.datasource
 
+import arrow.core.Either
+import com.picsart.business.arch.error.Empty
+import com.picsart.business.generators.dboPost
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.list
+import io.kotest.property.arbitrary.next
 
 class DatabasePostsDataSourceTest : BehaviorSpec({
 
     /*Test cases:
-    * - it returns either Result.success or Result.failure
     * - if there is no stored data it returns a DataEmpty Failure
     * - if there is stored data then the data source returns it
-    * - DataSource does not persist data over recreation */
+    * - It can store data*/
 
+
+    Given("DatabasePostsDataSource") {
+        val sut = DatabasePostsDataSource()
+        When("storing") {
+            val posts = Arb.list(Arb.dboPost()).next()
+            val result = sut(posts)
+            Then("It returns the last saved value") {
+                result shouldBe sut()
+            }
+        }
+        When("Getting the value") {
+            And("The storage is empty") {
+                sut(emptyList())
+                val result = sut()
+                Then("The result is an Empty Failure") {
+                    result shouldBe Either.Left(Empty)
+                }
+            }
+            And("The storage is not empty") {
+                val posts = Arb.list(Arb.dboPost()).next()
+                sut(posts)
+                val result = sut()
+                Then("The result is the expected list of posts") {
+                    result shouldBe Either.Right(posts)
+                }
+            }
+        }
+    }
 })
