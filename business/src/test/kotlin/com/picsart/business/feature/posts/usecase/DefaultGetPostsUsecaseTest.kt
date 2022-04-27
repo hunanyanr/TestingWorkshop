3648bbae699380aca630a3950aea3d823fe0d4bd
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2022-04-27 19:32:35 +0200

    test(workshop): changed test cases for usecase

--- business/src/test/kotlin/com/picsart/business/feature/posts/usecase/DefaultGetPostsUsecaseTest.kt
+++ business/src/test/kotlin/com/picsart/business/feature/posts/usecase/DefaultGetPostsUsecaseTest.kt
@@ -8,7 +8,8 @@ class DefaultGetPostsUsecaseTest() : BehaviorSpec({
     *  - It returns either left or right
     *  - It propagates the result of the repository
     *  - It propagates the failure of the repository
-    *  - If a post title starts by Consonant all title is uppercase
-    *  - If a post title starts by Vocal all title is lowercase
+    *  - It filters posts without title
     * */
+
+
 })
